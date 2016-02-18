51ad739aa23b18d5f1726e18849cc2955dbaf116
Jakub Tesárek <mail@jakubtesarek.cz>
2016-02-18 23:05:37 +0100

    remove support for obscure python versions

--- kwargs/kwargs.py
+++ kwargs/kwargs.py
@@ -1,6 +1,5 @@
-# coding=utf8
 #pylint: disable=too-few-public-methods
-u""" Kwargs is the only True python micro-framework that doesn't limit your creativity™. """
+""" Kwargs is the only True python micro-framework that doesn't limit your creativity™. """
 
 def run(callback, *args, **kwargs):
     """ Alias for App.run """
